2bb4ec26190ffb09f7dbbc3dd0cfa5a61394cdea
github username <mendelblesofsky@gmail.com>
2016-01-10 16:43:49 -0500

    and behold my field of commit messages, it lay barren

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+	attr_accessor :fibos
+
+	def initialize(num)
+		@fibos = get_fibos(num)
+	end
+
+	def get_fibos(num, arr=[1,2])
+		if arr[-1] >= num
+			arr.pop ; return arr
+		end
+		arr << (arr[-1] + arr[-2])
+		get_fibos(num, arr)
+	end
+
+	def sum
+		fibos.select {|num| num % 2 == 0 }.inject(&:+)
+	end
+
+end
