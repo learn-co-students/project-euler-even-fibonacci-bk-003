53aa7a2f44c77d6d338926239278561bc2226335
Stephen Koltz <steve.koltz@gmail.com>
2016-02-04 10:41:12 -0500

    rubying

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+require 'pry' # Implement your object-oriented solution here!
+class EvenFibonacci
+	def initialize(num)
+		@num = num
+		@fibo = [1,2]
+	end
+
+	def fibo 
+		while @fibo[-1] + @fibo[-2] <= @num
+			@fibo << @fibo[-1] + @fibo[-2]
+		end
+		@fibo
+	end
+
+	def sum 
+		fibo 
+		final = 0
+		@fibo.select {|x| x%2==0}.each {|y| final+= y}
+		final
+	end
+
+end
